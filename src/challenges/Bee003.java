e9406f3db295920cc1b2f8fe50e1e95b42480031
Gabriel Rodrigues <gabers357@gmail.com>
2024-02-08 10:39:55 -0300

    Add .classpath and .project files, and Bee003.java to the repository

---
+++ src/challenges/Bee003.java
@@ -0,0 +1,15 @@
+package challenges;
+import java.util.Scanner;
+
+public class Bee003 {
+
+	public static void main(String[] args) {
+		Scanner inputValues = new Scanner(System.in);
+		int firstValue, secondValue, result ;
+		firstValue = inputValues.nextInt();
+		secondValue = inputValues.nextInt();
+		result = firstValue + secondValue;
+		System.out.printf("Soma = "+ result);
+	}
+
+}
